f13222a88955de5b4d3d723cb110e80491bcc5e2
JasfferTP <69882335+Jasffertp@users.noreply.github.com>
2022-02-12 16:16:08 +0800

    Add files via upload

---
+++ equipment.php
@@ -0,0 +1,28 @@
+<head>
+	<title>Equipment Inventory</title>
+</head>
+<?php
+	session_start();
+	include 'header.php';
+?>
+<div class="container-fluid py-4 overflow-hidden">
+	<table class="table rounded-3 shadow-lg table-hover mb-5">
+	  <thead class="thead-dark">
+		<tr>
+		  <th scope="col">Equipment</th>
+		  <th scope="col">Asset</th>
+		  <th scope="col">Floor</th>
+		  <th scope="col">Room number</th>
+		  <th scope="col">Date installed</th>
+		  <th scope="col">Condition</th>
+		  <th scope="col">Operating</th>
+		</tr>
+	  </thead>
+	  <tbody>
+	  
+		<?php
+			include 'backend/get_equipment.p.php';
+		?>
+	  </tbody>
+	</table>
+</div>
---
+++ reports.php
@@ -0,0 +1,126 @@
+<head>
+	<title>Reports</title>
+	<script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
+</head>
+<?php
+	session_start();
+	include 'header.php';
+?>
+
+<div class="container-fluid py-4 overflow-hidden">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col p-2">
+				<div class="btn-group">
+				  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Action
+				  </button>
+				  <div class="dropdown-menu">
+					<a class="dropdown-item" href="#">Action</a>
+					<a class="dropdown-item" href="#">Another action</a>
+					<a class="dropdown-item" href="#">Something else here</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Separated link</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col p-2">
+				<div class="dropdown">
+				  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Report Status
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<a class="dropdown-item" href="#">Action</a>
+					<a class="dropdown-item" href="#">Another action</a>
+					<a class="dropdown-item" href="#">Something else here</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col p-2">
+				<div class="dropdown">
+				  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					For repair
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<a class="dropdown-item" href="#">Action</a>
+					<a class="dropdown-item" href="#">Another action</a>
+					<a class="dropdown-item" href="#">Something else here</a>
+				  </div>
+				</div>
+			</div>
+			<div class="col p-2">
+				<div class="btn-group btn-group" role="group">
+			  <a type="button" class="btn btn-info 
+				<?php
+					if(!isset($_GET['time'])){
+						echo 'active';
+					}else if($_GET['time'] == 'day'){
+						echo 'active';
+					}
+				?>
+			  " href="reports.php?site=Reports&page=1&time=day">Daily</a>
+			  <a type="button" class="btn btn-info <?php
+					if(isset($_GET['time'])){
+						if($_GET['time'] == 'week'){
+							echo 'active';
+						}
+					}
+				?>" href="reports.php?site=Reports&page=1&time=week">This Week</a>
+			  <a type="button" class="btn btn-info <?php
+					if(isset($_GET['time'])){
+						if($_GET['time'] == 'month'){
+							echo 'active';
+						}
+					}
+				?>" href="reports.php?site=Reports&page=1&time=month">This Month</a>
+				
+				<a type="button" class="btn btn-info <?php
+					if(isset($_GET['time'])){
+						if($_GET['time'] == 'year'){
+							echo 'active';
+						}
+					}
+				?>" href="reports.php?site=Reports&page=1&time=year">This year</a>
+			</div>
+			</div>
+			<div class="col p-2">
+				<form class="form-inline">
+					<input class="form-control mr-sm-2 w-100" type="search" placeholder="Search" aria-label="Search">
+				</form>
+			</div>
+			
+			
+			
+			
+			
+		</div>
+	</div>
+	
+	<table class="table rounded-3 shadow-lg table-hover mb-5">
+	  <thead class="thead-dark">
+		<tr>
+		  <th scope="col">Task</th>
+		  <th scope="col">Equipment</th>
+		  <th scope="col">Floor</th>
+		  <th scope="col">Room Number</th>
+		  <th scope="col">Report status</th>
+		  <th scope="col">Date submitted</th>
+		  <th scope="col">For repair</th>
+		  <th scope="col">Submitted by</th>
+		</tr>
+	  </thead>
+	  <tbody>
+	  
+		<?php
+			include 'backend/get_reports.p.php';
+		?>
+	  </tbody>
+	</table>
+	
+	<?php
+		include 'backend/table_pagination_reports.p.php';
+	?>
+	
+</div>
+

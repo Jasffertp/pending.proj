3355ba4c2a05856f35b90a196cc98c210bd30b0c
DRPC <60638686+DemeterCaubang@users.noreply.github.com>
2022-03-03 14:10:51 +0800

    Add files via upload

--- backend/get_status_report_genset.p.php
+++ backend/get_status_report_genset.p.php
@@ -20,7 +20,6 @@ if(isset($_POST['submit']))
     $fuel_level = $_POST['fuel_level'];
     $repair_remarks = $_POST['repair_remarks'];
     $other_remarks = $_POST['other_remarks'];
-    $date_created = $_GET['d_id'];
     $report_status = "done";
 
     date_default_timezone_set('Asia/Hong_Kong');
@@ -50,27 +49,34 @@ if(isset($_POST['submit']))
         $gas_leak = $_POST['gas_leak'];
     }
 
-	$gensetReadings = "INSERT into `equipment_readings_genset` (`equipment_id`,`report_id`, `voltage_line_1`, `voltage_line_2`, `voltage_line_3`, `current_line_1`, `current_line_2`, `current_line_3`, `frequency`, `battery_voltage`, `running_hours`, `oil_pressure`, `oil_temperature`, `rotation`, `fuel_level`, `abnormal_sound`, `gas_leak`, `for_repair`, `repair_remarks`, `other_remarks`, `date_created`, `time_submitted`, `assigned_to`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	$gensetReadings = "INSERT into `equipment_readings_genset` (`equipment_id`,`report_id`, `voltage_line_1`, `voltage_line_2`, `voltage_line_3`, `current_line_1`, `current_line_2`, `current_line_3`, `frequency`, `battery_voltage`, `running_hours`, `oil_pressure`, `oil_temperature`, `rotation`, `fuel_level`, `abnormal_sound`, `gas_leak`, `for_repair`, `repair_remarks`, `other_remarks`, `date_created`, `assigned_to`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
 	if(!mysqli_stmt_prepare($stmt, $gensetReadings)){
 		header("Location: ../createStatusReport.php?&error=insert%20genset%20readings");
 		exit();
 	}else{
-		mysqli_stmt_bind_param($stmt, "iidddddddddidiissssssss", $e_id, $r_id, $v1, $v2, $v3, $c1, $c2, $c3, $frequency, $battery_voltage, $running_hours, $oil_pressure, $oil_temperature, $rotation, $fuel_level, $abnormal_sound, $gas_leak, $for_repair, $repair_remarks, $other_remarks, $date_created, $time_submitted, $_SESSION['userId']);
+		mysqli_stmt_bind_param($stmt, "iidddddddddidiisssssss", $e_id, $r_id, $v1, $v2, $v3, $c1, $c2, $c3, $frequency, $battery_voltage, $running_hours, $oil_pressure, $oil_temperature, $rotation, $fuel_level, $abnormal_sound, $gas_leak, $for_repair, $repair_remarks, $other_remarks, $time_submitted, $_SESSION['userId']);
 		mysqli_stmt_execute($stmt);
 		}
 
     // update report status to done
-    
-    /*
-    $sql = "UPDATE `reports` SET `date_submitted` = $time_submitted WHERE `report_id` = ".$r_id."";
-	$stmt = mysqli_stmt_init($conn);
-	
-	if(!mysqli_stmt_prepare($stmt, $sql)){
-		echo 'error updating reports database';
-	}else{
-		mysqli_query($conn, $sql);
+    $sql = "UPDATE `reports` SET `date_submitted` = '".$time_submitted."',`report_status` = '".$report_status."'  WHERE `report_id` = ".$r_id."";
+    $stmt = mysqli_stmt_init($conn);
+        
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        echo 'error updating reports database';
+    }else{
+        mysqli_query($conn, $sql);
+        //echo 'reports date submitted set';
     }
-    */
+        
+    $stmt = mysqli_stmt_init($conn);
+        
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        //header('Location:../users.php?updatenotsuccessful&page=1&site=Users');
+        exit();
+    }else{
+        $result = mysqli_query($conn,$sql);
+    }
 }
 ?>
--- backend/get_status_report_hvac.p.php
+++ backend/get_status_report_hvac.p.php
@@ -10,7 +10,6 @@ if(isset($_POST['submit']))
 	$temp = $_POST['temp'];
     $repair_remarks = $_POST['repair_remarks'];
     $other_remarks = $_POST['other_remarks'];
-    $date_created = $_GET['d_id'];
     $report_status = "done";
 
     date_default_timezone_set('Asia/Hong_Kong');
@@ -26,27 +25,34 @@ if(isset($_POST['submit']))
         $for_repair = $_POST['for_repair'];
     }
     
-    $airconReadings = "INSERT into `equipment_readings_aircon` (`equipment_id`,`report_id`, `volt`, `pressure`, `temp`, `for_repair`, `repair_remarks`, `other_remarks`, `date_created`, `time_submitted`, `assigned_to`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+    $airconReadings = "INSERT into `equipment_readings_aircon` (`equipment_id`,`report_id`, `volt`, `pressure`, `temp`, `for_repair`, `repair_remarks`, `other_remarks`, `date_created`, `assigned_to`) VALUES (?,?,?,?,?,?,?,?,?,?)";
         
     if(!mysqli_stmt_prepare($stmt, $airconReadings)){
         header("Location: ../createStatusReport.php?&error=insert%20hvac%20readings");
         exit();
     }else{
-        mysqli_stmt_bind_param($stmt, "iiiidssssss", $e_id, $r_id, $volt, $pressure, $temp, $for_repair, $repair_remarks, $other_remarks, $date_created, $time_submitted, $_SESSION['userId']);
+        mysqli_stmt_bind_param($stmt, "iiiidsssss", $e_id, $r_id, $volt, $pressure, $temp, $for_repair, $repair_remarks, $other_remarks, $time_submitted, $_SESSION['userId']);
         mysqli_stmt_execute($stmt);
     }
-    
+
     // update report status to done
-    
-    /*
-    $sql = "UPDATE `reports` SET `date_submitted` = $time_submitted WHERE `report_id` = ".$r_id."";
+    $sql = "UPDATE `reports` SET `date_submitted` = '".$time_submitted."',`report_status` = '".$report_status."', `for_repair` = ".$for_repair."  WHERE `report_id` = ".$r_id."";
 	$stmt = mysqli_stmt_init($conn);
 	
 	if(!mysqli_stmt_prepare($stmt, $sql)){
 		echo 'error updating reports database';
 	}else{
 		mysqli_query($conn, $sql);
+        //echo 'reports date submitted set';
     }
-    */
+	
+	$stmt = mysqli_stmt_init($conn);
+	
+	if(!mysqli_stmt_prepare($stmt, $sql)){
+		//header('Location:../users.php?updatenotsuccessful&page=1&site=Users');
+		exit();
+	}else{
+		$result = mysqli_query($conn,$sql);
+    }
 }
 ?>
